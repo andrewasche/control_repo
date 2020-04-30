ecd62ae8af21c756e634dffb10a5f26ef5f52a2c
Andrew Asche <asche069@umn.edu>
2020-04-29 19:26:17 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -3,4 +3,14 @@ class profile::ssh_server{
     ensure => present,
   
   }
+  service { 'sshd':
+    ensure => 'running',
+    enable => 'true',
+  }
+  ssh_authorized_key { 'root@master.puppet.vm':
+    ensure => present,
+    user   => 'root',
+    type   => 'ssh-rsa',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDVar6xETMPymsRJti8kkqUk7AiM6YNuZwcPqMzTOl2mUyA/H6AC8kD1OxQJ2t02KZQAhhwfwTqbq9BJzQ4lq3SPBjOavyhF4ch/pcTBkr1GjXw8gyw+vX9yr00q+v8XBGAwtA0H30OVM6Rdd/uv/c9s+bFvlKfEbU05hqU4mP/67FeHGMZYzW/lez0d3H7i0NcPeFyY5aUum1aj2rLsWMf52Fnf/p59LL81XgcU+WaV0Ef8FJTg9IPPbYl6kpu1KYUw9LutER6WwiijDHMuUgF36hepj44BWIMJP7nJo1HKXbnBeAJUigiQcbBs1RkKu80NlIHKgLv32Jnv8zlWwMZ',
+  }
 }
