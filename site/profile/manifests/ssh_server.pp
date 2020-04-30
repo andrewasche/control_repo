4e06af6d3d1722a8872d176c357698035c8b4e92
Andrew Asche <asche069@umn.edu>
2020-04-29 19:40:37 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -11,6 +11,6 @@ class profile::ssh_server{
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDVar6xETMPymsRJti8kkqUk7AiM6YNuZwcPqMzTOl2mUyA/H6AC8kD1OxQJ2t02KZQAhhwfwTqbq9BJzQ4lq3SPBjOavyhF4ch/pcTBkr1GjXw8gyw+vX9yr00q+v8XBGAwtA0H30OVM6Rdd/uv/c9s+bFvlKfEbU05hqU4mP/67FeHGMZYzW/lez0d3H7i0NcPeFyY5aUum1aj2rLsWMf52Fnf/p59LL81XgcU+WaV0Ef8FJTg9IPPbYl6kpu1KYUw9LutER6WwiijDHMuUgF36hepj44BWIMJP7nJo1HKXbnBeAJUigiQcbBs1RkKu80NlIHKgLv32Jnv8zlWwMZ',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCkT/9N469ytKi8K8+uhdyQPeNrVtM/FG7RxOo/USBi1aXkfNcnRIPbmfFcjZ4cQsby2g7klNWtyChS/WHyLQ7I/NSxURdRf4rqoXUuQAZT+9wip7MeINZw7m7JtAqOj5S9ARycWUGESj0UAdXt+3cdvbJIabFbNhJXTe04ODsDKUvHbZrA28W7elQr9zw+A+M0Ey9Nfu5qZsjHdee6wAfUIBvSmtP6BQdXNhoggEfXaVkiijdc0Maau6oJ4EJgYApUmPpi4jN0LIZKuEpPk6iFjTw7Hu5ZjhJqA4E7DgAlzPT9LLUaeSFwxqV96KqGnbXtK2rNnqqwn/CI0I5f+U03',
   }
 }
